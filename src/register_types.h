454fcb20793dc2abf5aa7295cfd9ba867223d31e
Alecx_23 <alexmiron977@gmail.com>
2026-04-18 22:08:46 +0300

    Deleted Everything

--- src/register_types.h
+++
@@ -1,6 +0,0 @@
-﻿#pragma once
-#include <godot_cpp/core/class_db.hpp>
-
-extern "C" {
-GDExtensionBool GDE_EXPORT initialize_inventory_system_module(GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization);
-}
